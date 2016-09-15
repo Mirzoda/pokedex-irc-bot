b6d7a48962e71cdcfaf75e41349e4226c4d7c7d9
jerodev <jeroen@deviaene.eu>
2016-09-15 13:19:32 +0200

    Added tests for the EightBall command

---
+++ test/commands/EightBall.js
@@ -0,0 +1,23 @@
+/* global describe, it */
+
+var expect = require('expect.js');
+
+// Load an eightball instance
+var eightball = require("../../dist/commands/EightBall.js")["default"];
+
+describe('Commands/EightBall', function () {
+
+    it('should cache results', function (done) {
+
+        eightball.doCommand("!kan dit?", "mocha", "#test", function (result) {
+            expect(result).to.be.a('string');
+
+            eightball.doCommand("!kan dit?", "mocha", "#test", function (result2) {
+                expect(result).to.be(result2);
+                done();
+            });
+        });
+
+    });
+
+});
