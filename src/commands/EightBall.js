6cb54529f24ef468933a23cf5f4f0712f3eb27a8
jerodev <jeroen@deviaene.eu>
2016-10-21 11:24:38 +0200

    Added the 'when' question to pokedex

--- src/commands/EightBall.js
+++ src/commands/EightBall.js
@@ -1,8 +1,31 @@
+const _ = require('underscore');
+
 import Cache from '../common/Cache.js';
 
 export default class EightBall {
 
     static doCommand(msg, from, to, callBack) {
+        if (msg.match(/^!wanneer/)) {
+            callBack(
+                Cache.instance.remember("eightball_" + encodeURI(msg), 15, function () {
+                    return _.sample([
+                        _.random(2, 6) + ' maand geleden',
+                        'Gisteren',
+                        'Ik dacht dat dat morgen was.',
+                        'Lang voor uwen tijd',
+                        'Misschien ooit, wie weet...',
+                        'Morgen',
+                        'NEVAH!',
+                        'Nooit',
+                        'Over ' + _.random(2, 6) + ' maand ongeveer',
+                        'Was dat gisteren niet?'
+                    ]);
+                })
+            );
+
+            return;
+        }
+
 
         callBack(
             Cache.instance.remember("eightball_" + encodeURI(msg), 15, function () {
@@ -19,7 +42,7 @@ export default class EightBall {
                     "Nee!"
                 ];
 
-                return responses[Math.floor(Math.random() * responses.length)];
+                return _.sample(responses);
 
             })
         );
@@ -34,8 +57,10 @@ export default class EightBall {
             "^!moet ",
             "^!wil ",
             "^!zal ",
-            "^!zou "
-        ]
+            "^!zou ",
+
+            "^!wanneer "
+        ];
     }
 
 }
